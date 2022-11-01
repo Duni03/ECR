ad70348553e5fc727dac987c4c441dc1afff1e24
Duni03 <61987129+Duni03@users.noreply.github.com>
2022-11-01 14:45:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Nihal'),
+        body: JSON.stringify('Hello from D Nihal'),
     };
     return response;
 };
